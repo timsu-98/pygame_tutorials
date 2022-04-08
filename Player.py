a076e9bfbb2805675062726c7c1a7a863c0122ca
timsu <tim.su@tum.de>
2022-04-08 16:15:53 +0200

    update Player.py pygame_tutorials.py

--- Player.py
+++ Player.py
@@ -1,25 +1,48 @@
+from ctypes import util
+from turtle import screensize
 import pygame
 import utils
+import random
+import colors
 
 class Snail(pygame.sprite.Sprite):
-    def __init__(self, orientation = utils.UP) -> None:
+    def __init__(self, params: dict) -> None:
         super().__init__()
         self.image = pygame.image.load("./media/snail.png")
-        self.rect = pygame.Rect(320, 180, 30, 30)
-        self.rect.center = (320, 180)
-        self.orientation = orientation
+        self.rect = pygame.Rect(10.0, 10.0, 592.0, 592.0)
+        # self.rect.center = (320, 180)
+        self.params = params
 
     def update(self) -> None:
         pressed_keys = pygame.key.get_pressed()
 
-        if self.rect.left > 0:
-            if pressed_keys[pygame.K_LEFT]:
-                self.rect.move_ip(-10, 0)
-            if pressed_keys[pygame.K_RIGHT]:
-                self.rect.move_ip(10, 0)
+        if self.rect.left > 0 and pressed_keys[pygame.K_LEFT]:
+            self.rect.move_ip(-self.params["VEL_NORMAL"], 0)
+        if self.rect.right < self.params["SCREEN_WIDTH"] and pressed_keys[pygame.K_RIGHT]:
+            self.rect.move_ip(self.params["VEL_NORMAL"], 0)
+        if self.rect.top > 0 and pressed_keys[pygame.K_UP]:
+            self.rect.move_ip(0, -self.params["VEL_NORMAL"])
+        if self.rect.bottom < self.params["SCREEN_HEIGHT"] and pressed_keys[pygame.K_DOWN]:
+            self.rect.move_ip(0, self.params["VEL_NORMAL"])
     
     def draw(self, screen: pygame.Surface) -> None:
         assert type(screen)==pygame.Surface
         screen.blit(self.image, self.rect)
 
-        
+class Bear(pygame.sprite.Sprite):
+    def __init__(self, params: dict) -> None:
+        super().__init__()
+        self.image = pygame.image.load("./media/bear.png")
+        self.rect = pygame.Rect(320.0, 180.0, 216.0, 233.0)
+        # self.rect.center = (640, 360)
+        self.params = params
+    
+    def move(self) -> None:
+        self.rect.move_ip(0, self.params["VEL_FAST"])
+        if (self.rect.bottom > 1080):
+            self.rect.center = (640, 0)
+    
+    def draw(self, screen: pygame.Surface) -> None:
+        screen.blit(self.image, self.rect)
+        if self.params["DRAW_RECT"]:
+            pygame.draw.rect(screen, colors.RED, self.rect, self.rect.width)
--- pygame_tutorials.py
+++ pygame_tutorials.py
@@ -1,16 +1,31 @@
+from operator import truediv
 from numpy import blackman, place
 import pygame
 import sys
 import colors
-from Player import Snail
+from Player import Bear, Snail
+
+# define all global variables here
+SCREEN_WIDTH = 1920
+SCREEN_HEIGHT = 1080
+
+FPS = 60
+
+PARAMS = {
+    "VEL_NORMAL":       5.0,
+    "VEL_FAST"  :       10.0,
+    "VEL_SLOW"  :       2.5,
+    "SCREEN_WIDTH" :    1920,
+    "SCREEN_HEIGHT" :   1080,
+    "DRAW_RECT":        False
+}
 
 def main():
     pygame.init()
 
     if pygame.get_init():
         print("\033[1;32mPygame is correctly initialized.\033[0m")
-
-    FPS = 60
+        
     frame_per_sec = pygame.time.Clock()
 
     logo = pygame.image.load("./media/logo.png")
@@ -19,7 +34,7 @@ def main():
 
     pygame.display.set_caption("Pygame Tutorials")
 
-    screen = pygame.display.set_mode((640, 360))
+    screen = pygame.display.set_mode((PARAMS["SCREEN_WIDTH"], PARAMS["SCREEN_HEIGHT"]))
     screen.fill(colors.WHITE)
     print(type(screen))
 
@@ -27,8 +42,9 @@ def main():
 
     draw_point = [320, 180]
 
-    snail = Snail()
+    snail = Snail(params=PARAMS)
 
+    bear = Bear(params=PARAMS)
 
     # Game loop
     while running:
@@ -46,19 +62,32 @@ def main():
                     pygame.draw.circle(screen, colors.BLUE, draw_point, 5)
                     draw_point[0] += 10
                     draw_point[1] += 10
+                
+                # For debugging
+                if event.key == pygame.K_p:
+                    print("Printing debugging info...")
+                    print(f"The snail coordinates are:   ({snail.rect.left: .2f}, {snail.rect.top: .2f}")
+                    print(f"The bear  coordinates are:   ({snail.rect.left: .2f}, {snail.rect.top: .2f}")
+
+
             # Keyup
             if event.type == pygame.KEYUP:
                 print("Key released.")
                 if event.key == pygame.K_SPACE:
                     print("Space bar released.")
+
+        # update all characters
         snail.update()
+        bear.move()
+
+        # draw everything to the screen
+        screen.fill(colors.WHITE)
+        bear.draw(screen)
         snail.draw(screen)
 
+        # push the image to display
         pygame.display.update()
         frame_per_sec.tick(FPS)
-
-        
-    
 
 if __name__=="__main__":
     main()
